30644a7d90258f13580d40cc10f862b225693de1
b27676110 <105021036@live.asia.edu.tw>
2016-11-06 00:02:44 +0800

    105021036

--- src/hw/hw05.java
+++ src/hw/hw05.java
@@ -6,14 +6,27 @@ public class hw05 {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		int n = scn.nextInt();
-		int [ ][ ] data = new int[n][n];
+		int data [ ][ ]  = new int[n][n];
+		int sum=1;
+		
+		for(int i =0;i<n;i++){
+			for(int j =0;j<=i;j++){
+				data[i-j][j]=sum;
+				sum++;
+			}
+		}	
+		for(int i =1;i<n;i++){
+			for(int j =0;j<n-i;j++){
+				data[n-j-1][i+j]=sum;
+				sum++;
+			}
+		}
 		for(int i = 0;i<n;i++){
 			for(int j = 0;j<n;j++){
-				System.out.print(data[i][j]);
+				System.out.print(data[i][j]+" ");
 			}
 			System.out.println("");
 		}
 
 	}
-
 }
